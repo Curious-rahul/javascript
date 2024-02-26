d28081a6ef55348d2ab1e9c59b629d63e88da29f
Curious-rahul <99556541+Curious-rahul@users.noreply.github.com>
2024-02-26 18:38:49 +0000

    javascript changes

---
+++ 01_basic/conversionoper.js
@@ -0,0 +1,11 @@
+let score = "33"
+//console.log(typeof score);
+
+let valueInNumber = Number(score)
+
+//console.log(valueInNumber);
+
+
+let loggedIn = "Rahul"
+let booleanLoggedIn = Boolean(loggedIn)
+console.log(booleanLoggedIn);
---
+++ 01_basic/variable.js
@@ -0,0 +1,6 @@
+const accountId =12345
+let accountNo = "1975637382638"
+var city = "bengaluru"
+state = "karnataka"
+
+console.table([accountId,accountNo,city,state])
