267b8724f88838b1b686e474f1a7494052b012cb
Curious-rahul <99556541+Curious-rahul@users.noreply.github.com>
2024-02-27 18:38:51 +0000

    stack and comparison

---
+++ 01_basic/stack.js
@@ -0,0 +1,19 @@
+// stack(primitive), Heap(Non-primitive)
+
+let myname = "rahul"
+let anothername = myname
+
+//anothername = "roy"
+console.log(myname);
+console.log(anothername);
+
+let userone = {
+    email : "rahul@gmail.com",
+    ph : "5740876550283"
+}
+let usertwo = userone
+//  usertwo = { 
+//     email : "roy@gmail.com"
+// }
+ console.log(userone);
+ console.log(usertwo);
